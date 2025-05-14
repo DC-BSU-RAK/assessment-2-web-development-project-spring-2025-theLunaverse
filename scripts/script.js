c0ab880d41f9de6346c4c8d6c4f6b00a49e09548
Luna Alsalman <manahel1506@gmail.com>
2025-05-14 22:39:39 +0400

    navigation functionality added

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // navigation functionality (for all pages)
+    const navToggle = document.getElementById('navToggle');
+    const navLinks = document.querySelector('.nav-links');
+
+            }
+        }
+    });
